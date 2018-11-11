b684cc41971f64f902ed03c83e652724e1a0d223
Joh Yoshida <johyoshida@gmail.com>
2018-11-10 17:53:23 -0800

    Testing routes and db

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require("express");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 const app = express();
+
+const dbconfig = require("./knexfile.js")[process.env.DB_ENV];
+const knex = require("knex")(dbconfig);
+
 
 app.get("/", (req, res) => {
   res.send("Habitat server");
 });
+
+app.get("/test", (req, res) => {
+  console.log(req.query.access_token);
+  knex("users").where({
+    email: "johyoshida@gmail.com"
+  }).then(rows => {
+    res.send(rows);
+  }).catch(err => {
+    res.send("Failed to get users")
+  })
+});
+
+app.post("/habitat", (req, res) => {
+  console.log(req.body);
+  res.send("Post recieved.");
+})
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
