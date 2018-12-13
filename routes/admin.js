a80eda76fecb06710cf8dd39e90457797efc6735
Joh Yoshida <johyoshida@gmail.com>
2018-12-12 18:49:26 -0800

    Skeleton

---
+++ routes/admin.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const moment = require("moment");
+const uuid = require("uuid/v1");
+
+// Database requirements
+const dbconfig = require("../knexfile.js")[process.env.DB_ENV];
+const knex = require("knex")(dbconfig);
+
+// Functions
+const { checkUserCredentials } = require("../lib/helpers");
+
+module.exports = () => {
+  const adminRoutes = express.Router();
+
+  return adminRoutes;
+}
