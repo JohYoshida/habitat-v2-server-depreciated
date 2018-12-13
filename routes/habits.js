14c7cda9656b8fae6316fc5770969dba37bd798b
Joh Yoshida <johyoshida@gmail.com>
2018-12-12 18:34:27 -0800

    GET habits

--- routes/habits.js
+++ routes/habits.js
@@ -12,5 +12,25 @@ const { checkUserCredentials } = require("../lib/helpers");
 module.exports = () => {
   const habitsRouter = express.Router();
 
+  // Return an array of habits belonging to a user
+  habitsRouter.get("/:user_id", (req, res) => {
+    checkUserCredentials(req.headers.authorization).then(result => {
+      if (result.id === req.params.user_id && result.verified) {
+        knex("habits")
+          .where({ user_id: req.params.user_id })
+          .then(rows => {
+            res.send(JSON.stringify(rows));
+          })
+          .catch(err => {
+            res.send("Failed to get habits!", err);
+            console.log("Error!", err);
+          });
+      } else
+        new Promise(function(resolve, reject) {
+          res.send("Failed to get habits!");
+        });
+    });
+  });
+
   return habitsRouter;
 }
