44b4141315ff93e5d8382b3fd2acf482a9a62080
Joh Yoshida <johyoshida@gmail.com>
2018-12-13 23:20:59 -0800

    Login/logout routes for admin area

--- routes/admin.js
+++ routes/admin.js
@@ -7,10 +7,29 @@ const dbconfig = require("../knexfile.js")[process.env.DB_ENV];
 const knex = require("knex")(dbconfig);
 
 // Functions
-const { checkUserCredentials } = require("../lib/helpers");
+const { checkUserCredentials, handleAuthHeader } = require("../lib/helpers");
 
 module.exports = () => {
   const adminRoutes = express.Router();
+
+  // Login
+  adminRoutes.get("/", (req, res) => {
+    const user = handleAuthHeader(req.headers.authorization);
+    if (user.email === "admin") {
+      checkUserCredentials(req.headers.authorization).then(result => {
+        req.session.isLoggedIn = true;
+        res.send(result);
+      });
+    } else {
+      res.send({ msg: "This is an admin only area!", verified: false });
+    }
+  });
+
+  // Logout
+  adminRoutes.post("/", (req, res) => {
+    req.session.isLoggedIn = false;
+    res.redirect("/")
+  });
 
   return adminRoutes;
 }
