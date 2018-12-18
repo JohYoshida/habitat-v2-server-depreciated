f213b81f98eab59083a88ffd2f69b8ba18772445
Joh Yoshida <johyoshida@gmail.com>
2018-12-18 00:55:54 -0800

    Post a new quote

--- routes/quotes.js
+++ routes/quotes.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const uuid = require("uuid/v1");
+const moment = require("moment");
 
 // Database requirements
 const dbconfig = require("../knexfile.js")[process.env.DB_ENV];
@@ -7,9 +9,10 @@ const knex = require("knex")(dbconfig);
 const { handleAuthHeader, checkUserCredentials } = require("../lib/helpers");
 
 module.exports = () => {
+  // Routes are prepended with /quotes
   const quotesRoutes = express.Router();
 
-  // Routes are prepended with /quotes
+  // Get an array of quotes belonging to a user
   quotesRoutes.get("/:user_id", (req, res) => {
     const { user_id } = req.params;
     checkUserCredentials(req.headers.authorization).then(user => {
@@ -24,9 +27,41 @@ module.exports = () => {
             console.log("Error!", err);
           });
       } else {
-        new Promise(function(resolve, reject) {
-          res.send("Failed to get quotes!");
+        new Promise((resolve, reject) => {
+          res.send("Authentication failed");
         })
+      }
+    })
+  });
+
+  // Post a new quote
+  quotesRoutes.post("/:user_id", (req, res) => {
+    const now = moment().format();
+    const id = uuid();
+    const { text, author, source } = req.body;
+    const { user_id } = req.params;
+    let quote = {
+      id,
+      user_id,
+      text,
+      author,
+      source,
+      createdAt: now,
+      modifiedAt: now,
+    };
+    checkUserCredentials(req.headers.authorization).then(user => {
+      if (user.id === user_id && user.verified) {
+        knex("quotes")
+          .insert(quote)
+          .then(() => res.send({ quote, msg: "Created new quote."}))
+          .catch(err => {
+            res.send("Failed to create quote!");
+            console.log("Error", err);
+          });
+      } else {
+        new Promise((resolve, reject) => {
+          res.send("Authentication failed");
+        });
       }
     })
   });
