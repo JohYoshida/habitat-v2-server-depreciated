1cd8b42d90b5aad07b2f74fe8e9a7d38c7696289
Joh Yoshida <johyoshida@gmail.com>
2018-12-18 16:40:24 -0800

    Edit quote

--- routes/quotes.js
+++ routes/quotes.js
@@ -29,9 +29,9 @@ module.exports = () => {
       } else {
         new Promise((resolve, reject) => {
           res.send("Authentication failed");
-        })
+        });
       }
-    })
+    });
   });
 
   // Post a new quote
@@ -47,24 +47,47 @@ module.exports = () => {
       author,
       source,
       createdAt: now,
-      modifiedAt: now,
+      modifiedAt: now
     };
     checkUserCredentials(req.headers.authorization).then(user => {
       if (user.id === user_id && user.verified) {
         knex("quotes")
           .insert(quote)
-          .then(() => res.send({ quote, msg: "Created new quote."}))
+          .then(() => res.send({ quote, msg: "Created new quote." }))
           .catch(err => {
-            res.send("Failed to create quote!");
+            res.send({ msg: "Failed to create quote!" });
             console.log("Error", err);
           });
       } else {
         new Promise((resolve, reject) => {
-          res.send("Authentication failed");
+          res.send({ msg: "Authentication failed" });
         });
       }
-    })
+    });
+  });
+
+  // Edit a quote
+  quotesRoutes.post("/:user_id/:quote_id", (req, res) => {
+    const modifiedAt = moment().format();
+    const { user_id, quote_id } = req.params;
+    const { text, author, source } = req.body;
+    checkUserCredentials(req.headers.authorization).then(user => {
+      if (user.id === user_id && user.verified) {
+        knex("quotes")
+          .where({ user_id, id: quote_id })
+          .update({ text, author, source, modifiedAt })
+          .then(rows => res.send({ msg: "Successfully edited quote" }))
+          .catch(err => {
+            res.send({ msg: "Failed to edit quote!" });
+            console.log("Error", err);
+          });
+      } else {
+        new Promise((resolve, reject) => {
+          res.send({ msg: "Authentication failed" });
+        });
+      }
+    });
   });
 
   return quotesRoutes;
-}
+};
